b34aa2c49065133bf07927cdb599962f47d90ac1
Marc-Antoine Loignon <developer@lognoz.org>
2019-03-16 10:53:48 -0400

    Package: Fix indent type into dwm package configuration

--- pkg/dwm/package.py
+++ pkg/dwm/package.py
@@ -10,63 +10,63 @@ static const Rule rules[] = {
 };'''
 
 def edit_package_build():
-   for line in edit_file("PKGBUILD"):
-      if line.startswith("depends=("):
-         print(line.replace(")", " 'adobe-source-code-pro-fonts')").replace("'st'", "'xterm'"))
-      else:
-         print(line)
+    for line in edit_file("PKGBUILD"):
+        if line.startswith("depends=("):
+            print(line.replace(")", " 'adobe-source-code-pro-fonts')").replace("'st'", "'xterm'"))
+        else:
+            print(line)
 
 def edit_config():
-   colors = {
-      "#000000": [ "selbgcolor", "selbordercolor", "normbgcolor", "normbordercolor" ],
-      "#D8D8D8": [ "normfgcolor" ],
-      "#FFFFFF": [ "selfgcolor" ]
-   }
+    colors = {
+        "#000000": [ "selbgcolor", "selbordercolor", "normbgcolor", "normbordercolor" ],
+        "#D8D8D8": [ "normfgcolor" ],
+        "#FFFFFF": [ "selfgcolor" ]
+    }
 
-   for line in edit_file("config.h"):
-      for color in colors:
-         for key in colors[color]:
-            if line.startswith("static const char " + key):
-               line = replace_ending('"', '"%s";' % color, line)
+    for line in edit_file("config.h"):
+        for color in colors:
+            for key in colors[color]:
+                if line.startswith("static const char " + key):
+                    line = replace_ending('"', '"%s";' % color, line)
 
-      if line.startswith("static const unsigned int borderpx"):
-         print(line.replace("1", "0"))
+        if line.startswith("static const unsigned int borderpx"):
+            print(line.replace("1", "0"))
 
-      elif line.startswith('static const char *tags'):
-         print(replace_ending("{", '{ "1", "2", "3", "4", "5" };', line))
-         print(rules)
+        elif line.startswith('static const char *tags'):
+            print(replace_ending("{", '{ "1", "2", "3", "4", "5" };', line))
+            print(rules)
 
-      elif line.startswith("#define SHCMD("):
-         print(line.replace("/bin/sh", "/bin/xterm"))
+        elif line.startswith("#define SHCMD("):
+            print(line.replace("/bin/sh", "/bin/xterm"))
 
-      elif line.startswith("static const char *termcmd"):
-         print(line.replace('"st"', '"xterm"'))
+        elif line.startswith("static const char *termcmd"):
+            print(line.replace('"st"', '"xterm"'))
 
-      elif "monospace" in line:
-         print(line.replace("monospace", "Source Code Pro"))
+        elif "monospace" in line:
+            print(line.replace("monospace", "Source Code Pro"))
 
-      else:
-         print(line)
+        else:
+            print(line)
 
 def remove_rules():
-   writing = True
+    writing = True
 
-   with open("config.h", "r") as input:
-      lines = input.readlines()
-      with open("config.h", "w") as output:
-         for line in lines:
-            if line.startswith("static const Rule rules[]"):
-               writing = False
+    with open("config.h", "r") as input:
+        lines = input.readlines()
+        with open("config.h", "w") as output:
+            for line in lines:
+                if line.startswith("static const Rule rules[]"):
+                    writing = False
 
-            if writing == False:
-               if line.startswith("/*"):
-                  writing = True
-               else:
-                  continue
+                if writing == False:
+                    if line.startswith("/*"):
+                        writing = True
+                else:
+                    continue
 
-            output.write(line)
+                output.write(line)
 
 def pre_build():
-   remove_rules()
-   edit_package_build()
-   edit_config()
+    remove_rules()
+    edit_package_build()
+    edit_config()
