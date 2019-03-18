ed4b3e94bd7f10f172e713e811ae1360ac754140
Marc-Antoine Loignon <developer@lognoz.org>
2019-03-18 00:39:28 -0400

    Package: Change line pkgver function return into firefox nightly PKGBUILD

--- pkg/firefox-nightly/package.py
+++ pkg/firefox-nightly/package.py
@@ -3,3 +3,10 @@
 
 name = "firefox-nightly"
 source = "https://aur.archlinux.org/firefox-nightly.git"
+
+def pre_build():
+    for line in edit_file("PKGBUILD"):
+        if line.strip().startswith("echo \"${_version}."):
+            print("  echo \"${_version}.$(date +%Y%m)\"")
+        else:
+            print(line)
