f1f3c05bbcee99af29bfcfbd44cbb6e40be32b24
Marc-Antoine Loignon <developer@lognoz.org>
2019-07-06 02:20:38 -0400

    Package: Change dwm package source

--- pkg/dwm/package.py
+++ pkg/dwm/package.py
@@ -2,11 +2,4 @@
 # -*- coding:utf-8 -*-
 
 name = "dwm"
-source = "https://aur.archlinux.org/dwm-git.git"
-
-def pre_build():
-    for line in edit_file("PKGBUILD"):
-        if line.startswith("pkgname="):
-            print("pkgname=dwm")
-        else:
-            print(line)
+source = "https://aur.archlinux.org/dwm.git"
