dd3a4b0b8096c2045931b70a83a1791978d4ad57
Marc-Antoine Loignon <developer@lognoz.org>
2019-12-12 19:32:10 -0500

    Package: Drop useless packages

--- pkg/koala-bin/package.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python
-# -*- coding:utf-8 -*-
-
-name = "koala-bin"
-source = "https://aur.archlinux.org/koala-bin.git"
--- pkg/ycmd-git/package.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python
-# -*- coding:utf-8 -*-
-
-name = "ycmd-git"
-source = "https://aur.archlinux.org/ycmd-git.git"
