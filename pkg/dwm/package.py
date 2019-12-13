dd5c582945e1b304808fb3dc1bfe6178b983a585
Marc-Antoine Loignon <developer@lognoz.org>
2019-12-12 20:02:51 -0500

    Package: Fix the name defined in package.py is the not the same in PKGBUILD

--- pkg/dwm/package.py
+++ pkg/dwm/package.py
@@ -1,5 +1,17 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
 
+import os
+
 name = "dwm"
 source = "https://aur.archlinux.org/dwm-git.git"
+
+
+def pre_build():
+    os.system("cp config.def.h config.h")
+
+    for line in edit_file("PKGBUILD"):
+        if line.startswith("pkgname="):
+            print("pkgname=dwm")
+        else:
+            print(line)
