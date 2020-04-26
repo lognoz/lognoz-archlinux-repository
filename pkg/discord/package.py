f5ed3213b25af4a5a0eb8af523af4bc4bb1cbbb6
Marc-Antoine Loignon <developer@lognoz.org>
2020-04-26 02:34:50 -0400

    Package: Drop discord, gconf and prepros packages

--- pkg/discord/package.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python
-# -*- coding:utf-8 -*-
-
-name = "discord"
-source = "https://aur.archlinux.org/discord.git"
--- pkg/prepros-bin/package.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python
-# -*- coding:utf-8 -*-
-
-name = "prepros-bin"
-source = "https://aur.archlinux.org/prepros-bin.git"
